7b762fd78814b7d767ef1a854caddd5de376770b
ptrmzk <visualgrading@gmail.com>
2017-07-29 03:55:07 -0400

    Corrected image import

--- src/components/content/OurStory.js
+++ src/components/content/OurStory.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import '../../stylesheets/Content.css';
-import '../../img/Lipman-E-01-14-17.jpg';
+import LizAndPiotr from '../../img/Lipman-E-01-14-17.jpg';
 
 class OurStory extends Component {
     constructor(props) {
@@ -11,12 +11,16 @@ class OurStory extends Component {
         return (
             <div className="content-ourstory">
                 <div className="content-ourstory-text">
+                    <div className="content-ourstory-image-wrapper">
+                        <img src={LizAndPiotr}
+                             className="content-ourstory-image" alt="Liz and Piotr in Black and White"/>
+                    </div>
                     Elizabeth and Piotr met at Northeastern University in 2007.
                     Elizabeth found Piotr on Facebook whilst looking for eligible Polish Honor's students.
                     They recently moved to New York City and are excited to start the next chapter of their adventure.
+
                 </div>
-                    <img src="../../img/Lipman-E-01-14-17.jpg"
-                         className="content-ourstory-image" alt="Liz and Piotr in Black and White"/>
+
 
             </div>
         );
